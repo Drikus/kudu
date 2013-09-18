4c8be27043d1683b1856c4ba3882def32026411f
Pranav K <prkrishn@hotmail.com>
2013-09-18 14:38:39 -0700

    signalr

--- Kudu.Services/Commands/PersistentCommandController.cs
+++ Kudu.Services/Commands/PersistentCommandController.cs
@@ -9,119 +9,85 @@ using Kudu.Contracts.Tracing;
 using Kudu.Core;
 using Microsoft.AspNet.SignalR;
 
+
 namespace Kudu.Services
 {
     public class PersistentCommandController : PersistentConnection
     {
+        private readonly ICommandExecutor _commandExecutor;
         private readonly ITracer _tracer;
-        private readonly IEnvironment _environment;
-        static readonly IDictionary<string, Process> Processes = new Dictionary<string, Process>();
+        private string _connectionId;
 
-        public PersistentCommandController(IEnvironment environment, ITracer tracer)
+        public PersistentCommandController(ICommandExecutor commandExecutor, ITracer tracer)
         {
-            _environment = environment;
+            _commandExecutor = commandExecutor;
+            _commandExecutor.CommandEvent += OnCommandEvent;
+
             _tracer = tracer;
         }
 
-        protected override Task OnConnected(IRequest request, string connectionId)
+        private void OnCommandEvent(CommandEvent evt)
         {
-            var process = GetProcessForConnection(connectionId);
-            var fmt = String.Format("Connected to {0} ({1})\n", process.ProcessName, process.Id);
-            return Connection.Send(connectionId, fmt);
-        }
-
-        protected override Task OnDisconnected(IRequest request, string connectionId)
-        {
-            try
+            if (evt.EventType == CommandEventType.Error)
             {
-                var process = GetProcessForConnection(connectionId, false);
-                if (process == null) return base.OnDisconnected(request, connectionId);
-
-                process.StandardInput.WriteLine("exit");
-                process.StandardInput.Flush();
-                Thread.Sleep(2000);
-                if (!process.HasExited)
-                {
-                    process.Kill();
-                }
+                Connection.Send(_connectionId, new { Error = evt.Data });
             }
-            catch (Exception exception)
+            else if (evt.EventType == CommandEventType.Output)
             {
-                _tracer.TraceError(exception);
-            }
-            return base.OnDisconnected(request, connectionId);
-        }
-
-        protected override Task OnReceived(IRequest request, string connectionId, string data)
-        {
-            return Task.Factory.StartNew(() =>
-            {
-                var process = GetProcessForConnection(connectionId);
-                process.StandardInput.WriteLine(data.Replace("\n", ""));
-                process.StandardInput.Flush();
-            });
-        }
-
-        Process GetProcessForConnection(string connectionId, bool createIfNotExist = true)
-        {
-            lock (Processes)
-            {
-                Process ret = null;
-                if (!Processes.TryGetValue(connectionId, out ret) && createIfNotExist)
-                {
-                    ret = StartProcess(connectionId);
-                    Processes.Add(connectionId, ret);
-                }
-                return ret;
+                Connection.Send(_connectionId, new { Output = evt.Data });
             }
         }
-        private Process StartProcess(string connectionId)
-        {
-            var startInfo = new ProcessStartInfo()
-            {
-                UseShellExecute = false,
-                FileName = System.Environment.ExpandEnvironmentVariables(@"%SystemDrive%\Windows\System32\cmd.exe"),
-                CreateNoWindow = true,
-                RedirectStandardError = true,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                WorkingDirectory = _environment.RootPath
-            };
 
-            var process = Process.Start(startInfo);
-            var outputReader = TextReader.Synchronized(process.StandardOutput);
-            process.ErrorDataReceived += (sender, evt) => Connection.Send(connectionId, new { Error = evt.Data });
-            process.Exited += (sender, evt) =>
+        protected override async Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            _connectionId = connectionId;
+            JObject input = JsonConvert.DeserializeObject<JObject>(data);
+            string command = input.Value<string>("command");
+            string workingDirectory = input.Value<string>("dir");
+            bool breakExecution = input.Value<bool>("break");
+
+            if (_commandExecutor.Executing)
             {
-                lock (Processes)
+                if (breakExecution)
                 {
-                    Processes.Remove(connectionId);
+                    _commandExecutor.CancelCommand();
                 }
-                Connection.Send(connectionId, new { Output = "Exited: " + process.ExitCode });
-            };
-            var outputThread = new Thread(() =>
+                else if (!String.IsNullOrEmpty(command))
+                {
+                    await _commandExecutor.SendInput(command);
+                }
+            }
+            else 
             {
-                while (!process.HasExited)
-                {
-                    int count;
-                    var buffer = new char[1024];
-                    while ((count = outputReader.Read(buffer, 0, 1024)) > 0)
-                    {
-                        var builder = new StringBuilder();
-                        builder.Append(buffer, 0, count);
-                        string s = builder.ToString();
-                        Connection.Send(connectionId, new { Output = s.Replace("\r\n", "\n")});
-                        buffer = new char[1024];
-                    }
-                    Thread.Sleep(200);
-                }
-            });
-            outputThread.Start();
-            process.EnableRaisingEvents = true;
-            process.BeginErrorReadLine();
-            return process;
+                await ExecProcess(connectionId, command, workingDirectory);
+            }
         }
 
-
+        private async Task ExecProcess(string connectionId, string command, string workingDirectory)
+        {
+            using (_tracer.Step("Executing " + command, new Dictionary<string, string> { { "CWD", workingDirectory } }))
+            {
+                object error = null;
+                try
+                {
+                    var result = await _commandExecutor.ExecuteCommandAsync(command, workingDirectory, calculateWorkingDir: true);
+                    await Connection.Send(connectionId, result);
+                }
+                catch (CommandLineException ex)
+                {
+                    _tracer.TraceError(ex);
+                    error = new { ex.ExitCode };
+                }
+                catch (Exception ex)
+                {
+                    _tracer.TraceError(ex);
+                    error = new { Error = ex.ToString(), ExitCode = -1 };
+                }
+                if (error != null)
+                {
+                    await Connection.Send(connectionId, error);
+                }
+            }
+        }
     }
 }
